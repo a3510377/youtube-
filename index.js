125cd301847586c0eb6ec75a1e755978b023522a
a3510377 <a102009102009@gmail.com>
2021-07-27 19:50:01 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const fetch = require('node-fetch')
+require("dotenv").config()
+const fs = require("fs")
+
+express()
+    .use(express.static("public"))
+    .get("/", (req, res) => {
+        fs.readFile(`${__dirname}/src/index.html`, "utf8", function (err, text) {
+            res.send(text)
+        })
+    })
+    .get("/api/comment", (req, res) => {
+        if ("videoId" in req.query) {
+            fetch(
+                `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=500&order=time&videoId=${req.query.videoId}&key=${process.env.googleApiToken}`
+            ).then((req) => req.json()).then((data) => {
+                if ("error" in data) {
+                    console.warn("error:", data.error.code, data.error.message)
+                }
+                res.send(data.items.map(function (item) {
+                    let msg = item.snippet.topLevelComment.snippet
+                    return {
+                        id: item.id,
+                        txtJs: msg.textOriginal,
+                        txtHtml: msg.textDisplay,
+                        authorName: msg.authorDisplayName,
+                        avatar: msg.authorProfileImageUrl,
+                        time: {
+                            send: msg.publishedAt,
+                            up: msg.updatedAt
+                        }
+                    }
+                }))
+            })
+        }
+    })
+    .listen(3001, () => {
+        console.log("web online")
+    })
